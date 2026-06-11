680dbcab31554fce40041c9e849cf9675074bd21
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-11 08:57:44 +0000

    [Finish]4-13

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_one_attached :profile_image
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   # 1:Nの関係を結ぶ
   has_many :post_images, dependent: :destroy
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
 end
