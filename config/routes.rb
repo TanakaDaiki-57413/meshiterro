4bd4e12a8a90a96cbacd0eab3509b9a5daa2ef34
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-11 06:52:33 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show]
+
+  resources :post_images, only: [:new,:create, :index, :show]
 
   resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
 
   resource :session
 
   resources :passwords, param: :token
-  
+
   root to: "homes#top"
   #名前付きルートをaboutに
   get "homes/about", to: "homes#about", as: "about"
